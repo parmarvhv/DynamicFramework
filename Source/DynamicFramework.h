a23b20f0f1b0272e13631f03e784278318945296
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-10 12:24:44 +0530

    add framework for iOS, tvOS, macOS

---
+++ Source/DynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  DynamicFramework.h
+//  DynamicFramework
+//
+//  Created by Vaibhav Parmar on 10/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicFramework.
+FOUNDATION_EXPORT double DynamicFrameworkVersionNumber;
+
+//! Project version string for DynamicFramework.
+FOUNDATION_EXPORT const unsigned char DynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicFramework/PublicHeader.h>
+
+
---
+++ Source/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  DynamicFramework
+//
+//  Created by Vaibhav Parmar on 10/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+import Foundation
+
+public class Model {
+    
+    public let devices: [String]
+    
+    public init() {
+        self.devices = ["iPhone", "iPad", "iPod"]
+    }
+    
+}
